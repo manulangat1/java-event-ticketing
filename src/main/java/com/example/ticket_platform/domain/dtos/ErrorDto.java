06614befa5df0935b76de6a5a8dca02c2e20fd2f
manulangat1 <emmanuelthedeveloper@gmail.com>
2025-12-10 23:44:21 +0300

    Adds error message field to error dto.

--- src/main/java/com/example/ticket_platform/domain/dtos/ErrorDto.java
+++ src/main/java/com/example/ticket_platform/domain/dtos/ErrorDto.java
@@ -4,9 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Optional;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ErrorDto {
     private  String error;
+    private Optional<String> errorMessage;
 }
