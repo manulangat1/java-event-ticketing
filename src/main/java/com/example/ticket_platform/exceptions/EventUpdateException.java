c1aa24fb3be8b1540d134222c7866f1678f72e0a
manulangat1 <emmanuelthedeveloper@gmail.com>
2025-12-17 17:35:27 +0300

    feat-event-update

---
+++ src/main/java/com/example/ticket_platform/exceptions/EventUpdateException.java
@@ -0,0 +1,7 @@
+package com.example.ticket_platform.exceptions;
+
+public class EventUpdateException extends RuntimeException {
+    public EventUpdateException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/example/ticket_platform/exceptions/TicketTypeNotFoundException.java
@@ -0,0 +1,23 @@
+package com.example.ticket_platform.exceptions;
+
+public class TicketTypeNotFoundException extends EventNotFoundException  {
+
+    public TicketTypeNotFoundException() {
+    }
+
+    public TicketTypeNotFoundException(String message) {
+        super(message);
+    }
+
+    public TicketTypeNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public TicketTypeNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public TicketTypeNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
